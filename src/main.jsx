8a61712520c5d79c7f05e7c04c550d146c73c543
Eneb <135270219+ametrahs@users.noreply.github.com>
2025-02-26 06:35:46 -0800

    prac

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+/* import {createRoot} from "react-dom/client"
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <h1> hello react</h1>
+) */
+// ** scrimba day-one
+// *** challenge-one
+/* import {createRoot} from "react-dom/client"
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <h1 style ={{color : "green"}}>you will be baletsega female in z world</h1>
+) */
+// *** challenge-two
+/* import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <ul>
+    <li>hi winner!</li>
+    <li>hi winner!</li>
+    <li>hi winner!</li>
+  </ul>
+) */
+// *** challenge-three
